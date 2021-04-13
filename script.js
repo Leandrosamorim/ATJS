cc4b16156572e714ff866e052186b6f46304d1bb
Leandro <leandro_samorim@hotmail.com>
2021-04-13 09:28:30 -0300

    Adaptação para o Pedro

--- script.js
+++ script.js
@@ -5,21 +5,29 @@ class Card {
     }
 }
 
-var files = ["img/android.png", "img/chrome.png", "img/facebook.png", "img/firefox.png", "img/googleplus.png", "img/html5.png", "img/twitter.png", "img/windows.png", "img/android.png", "img/chrome.png", "img/facebook.png", "img/firefox.png", "img/googleplus.png", "img/html5.png", "img/twitter.png", "img/windows.png"];
+var files = ["img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png"];
 files.sort(() => .5 - Math.random());
-var cards = files.map(x => x = new Card(x))
+var cards = files.map(x => x = new Card(x));
+var timer;
 
-function createTable() {
+async function createTable() {
 
+    timer = new Date();
     for (let i = 0; i < files.length; i = i + 4) {
         document.getElementById("board").innerHTML += `
         <tr>
-        <td><img class = "flip-card-inner" id = "${i}" src = ${cards[i].front} onclick = "OnClick(event)"></td>
-        <td><img class = "flip-card-inner" id = "${i + 1}" src = ${cards[i + 1].front} onclick="OnClick(event)"></td>
-        <td><img class = "flip-card-inner" id = "${i + 2}" src = ${cards[i + 2].front} onclick="OnClick(event)"></td>
-        <td><img class = "flip-card-inner" id = "${i + 3}" src = ${cards[i + 3].front} onclick="OnClick(event)"></td>
+        <td><img class = "flip-card-inner" id = "${i}" src = ${cards[i].back} onclick = "OnClick(event)"></td>
+        <td><img class = "flip-card-inner" id = "${i + 1}" src = ${cards[i + 1].back} onclick="OnClick(event)"></td>
+        <td><img class = "flip-card-inner" id = "${i + 2}" src = ${cards[i + 2].back} onclick="OnClick(event)"></td>
+        <td><img class = "flip-card-inner" id = "${i + 3}" src = ${cards[i + 3].back} onclick="OnClick(event)"></td>
         </tr>`;
     }
+    await sleep(1000);
+    for (let i = 0; i < cards.length; i++){
+        var card = document.getElementById(i);
+        card.src = cards[i].front;
+    }
+
     document.getElementById("start").style.visibility = "hidden";
 }
 
@@ -40,9 +48,12 @@ async function OnClick(event) {
 
         } else {
             document.getElementById("screen").style.visibility = "visible";
-            await sleep(1000)
+            var audio = document.getElementsByTagName("audio")[0];
+            audio.play();
+            await sleep(3000)
             .then(() => { document.getElementById("screen").style.visibility = "hidden"; })
-            .then(() => {TurnCard(card1); TurnCard(card2);}) 
+            await TurnCard(card1); 
+            await TurnCard(card2);
         }
         if(card1 && card2){
             card1 = false;
@@ -50,7 +61,13 @@ async function OnClick(event) {
         }
 
         if(document.getElementById("pts").innerHTML == "8"){
-            alert("Parabens, voce venceu!")
+            var time = new Date();
+            timer.setSeconds(time.getSeconds() - timer.getSeconds())
+            alert("Parabens, voce venceu em "+ timer.getSeconds() + " segundos!")
+            var audio = document.getElementsByTagName("audio")[0];
+            audio.src = "audio/parabens.mp3"
+            audio.play();
+            
         }
 
 
@@ -62,8 +79,8 @@ async function TurnCard(x) {
     console.log(x)
     var id = Number(x.id)
     x.style.transform += "rotateY(180deg)";
-    if (x.src == "file:///C:/Users/Leandro/Documents/Infnet/TPS/JavaScript/At/img/cross.png") {
-        x.src = cards[id].back;
+    if (x.src == "file:///C:/At/img/cross.png") {
+        x.setAttribute("src", cards[id].back);
         x.onclick = null;
     }else{
         x.src = cards[id].front;
